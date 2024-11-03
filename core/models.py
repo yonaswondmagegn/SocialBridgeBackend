cabfc1c1ba35b34e0f70e4226e9db1bdf19bfe96
yonaswondmagegn <yonaswondmagegn954@gmail.com>
2024-11-03 17:57:51 +0300

    fixint sesion in user

--- core/models.py
+++ core/models.py
@@ -5,28 +5,27 @@ from django.utils import timezone
 
 class UserManager(BaseManager):
 
-    def _create_user(self,phonenumber,session,password = None,**extra_fields):
+    def _create_user(self,phonenumber,password = None,**extra_fields):
         is_superuser = extra_fields.get('is_superuser')
         if not phonenumber:
             raise ValueError('phonenumber is Requred field')
-        if not session and not is_superuser:
-            raise ValueError('Session is Requred')
-        user = self.model(phonenumber = phonenumber,session = session,**extra_fields)
+       
+        user = self.model(phonenumber = phonenumber,**extra_fields)
         if  password:
             user.set_password(password)
         user.save(using = self._db)
         return user
     
-    def create_user(self, phonenumber,session,password =None, **extra_fields):
+    def create_user(self, phonenumber,password =None, **extra_fields):
         extra_fields.setdefault('is_staff',False) 
         extra_fields.setdefault('is_superuser',False)
-        return self._create_user(phonenumber,session,password,**extra_fields)
+        return self._create_user(phonenumber,password,**extra_fields)
     
-    def create_superuser(self, phonenumber,session,password = None,**extra_fields):
+    def create_superuser(self, phonenumber,password = None,**extra_fields):
         extra_fields.setdefault('is_active',True) 
         extra_fields.setdefault('is_staff',True)
         extra_fields.setdefault('is_superuser',True)
-        return self._create_user(phonenumber,session,password,**extra_fields)
+        return self._create_user(phonenumber,password,**extra_fields)
 
 
 class User(AbstractUser):
